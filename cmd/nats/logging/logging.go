e09b3b18edc252472022a3306037bc8ac4b376a5
Ian Marsman <ian@xmlteam.com>
2021-08-20 22:54:28 -0400

    Logging basically working

--- cmd/nats/logging/logging.go
+++ cmd/nats/logging/logging.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -13,13 +14,21 @@ import (
 
 // https://cloud.google.com/logging/docs/reference/libraries#linux-or-macos
 
+var cloudLogger *logging.Logger
 var logger *log.Logger
 var client *logging.Client
 var logName string
 
 // Logger get reference to logger
-func Logger() *log.Logger {
-	return logger
+// func Logger() *log.Logger {
+// 	return logger
+// }
+
+func flush() {
+	err := cloudLogger.Flush()
+	if err != nil {
+		fmt.Println("err", err)
+	}
 }
 
 func init() {
@@ -35,44 +44,56 @@ func init() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
+	// ctx2 := context.Background()
+	// err = client.Ping(ctx2)
+	// fmt.Println("error", err)
 	// Sets the name of the log to write to.
 	logName = config.Config().LogName
+	cloudLogger = client.Logger(logName)
 
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-	signal.Notify(stop, syscall.SIGTERM)
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt)
+		signal.Notify(stop, syscall.SIGTERM)
 
-	<-stop
+		fmt.Println("waiting")
+		<-stop
 
-	client.Close()
+		client.Close()
+	}()
 }
 
 // Debug make a debug log entry
 func Debug(msg ...string) {
-	logger = client.Logger(logName).StandardLogger(logging.Debug)
-	logger.Println(msg)
+	stdL := cloudLogger.StandardLogger(logging.Debug)
+	stdL.Println(msg)
+	// cloudLogger.Flush()
 }
 
 // Info make an info log entry
 func Info(msg ...string) {
-	logger = client.Logger(logName).StandardLogger(logging.Info)
-	logger.Println(msg)
+	stdL := cloudLogger.StandardLogger(logging.Info)
+	stdL.Println(msg)
+	// cloudLogger.Flush()
 }
 
 // Alert make an alert log entry
 func Alert(msg ...string) {
-	logger = client.Logger(logName).StandardLogger(logging.Alert)
-	logger.Println(msg)
+	stdL := cloudLogger.StandardLogger(logging.Alert)
+	stdL.Println(msg)
+	// cloudLogger.Flush()
 }
 
 // Warn log a warning entry
 func Warn(msg ...string) {
-	logger = client.Logger(logName).StandardLogger(logging.Warning)
-	logger.Println(msg)
+	stdL := cloudLogger.StandardLogger(logging.Warning)
+	stdL.Println(msg)
+	// cloudLogger.Flush()
 }
 
 // Error log an error entry
 func Error(msg ...string) {
-	logger = client.Logger(logName).StandardLogger(logging.Error)
-	logger.Println(msg)
+	stdL := cloudLogger.StandardLogger(logging.Error)
+	stdL.Println(msg)
+	// cloudLogger.Flush()
 }
---
+++ cmd/nats/logging/logging_test.go
@@ -0,0 +1,15 @@
+package logging
+
+import (
+	"testing"
+)
+
+// TestCall test call for numbered cartoon
+func TestCall(t *testing.T) {
+	// is := is.New(t)
+	Debug("hello debug log")
+	Info("hello info log")
+	Warn("hello warn log")
+	Error("hello error log")
+	flush()
+}
